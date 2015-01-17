70a76a336492d0abb757cae9126bd11c99e42f0c
Ronald Bellido <rbellido54@gmail.com>
2015-01-17 01:13:28 -0800

    Finished Lesson 9. Added rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "movies#index"
   get "/movies" => "movies#index"
-  get "/movie/:id" => "movies#show"
+  get "/movie/:id" => "movies#show", as: "movie"
 end
