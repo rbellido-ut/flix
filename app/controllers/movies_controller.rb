4c2e05c8986b2f14b50b947e1893677c9ebb60f6
Ronald Bellido <rbellido54@gmail.com>
2015-02-08 14:07:41 -0800

    Validations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,33 +6,40 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
-  
+
   def edit
     @movie = Movie.find(params[:id])
   end
-  
+
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to @movie
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie successfully updated!"
+    else
+      render :edit
+    end
   end
 
   def new
     @movie = Movie.new
   end
-  
+
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to @movie
+
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new
+    end
   end
-  
+
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url
+    redirect_to movies_url, alert: "#{@movie.title} removed"
   end
-  
+
 private
 
   def movie_params
