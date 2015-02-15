57bc94fd4dd47bee29d7c1968f2aa838a604b505
Ronald Bellido <rbellido54@gmail.com>
2015-02-14 17:32:01 -0800

    Lessons and review

--- spec/features/delete_movie_spec.rb
+++ spec/features/delete_movie_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe "Deleting a movie" do 
+describe "Deleting a movie" do
   it "destroys the movie and shows the movie listing without the deleted movie" do
     movie = Movie.create(movie_attributes)
-    
+
     visit movie_path(movie)
-    
+
     click_link 'Delete'
-    
+
     expect(current_path).to eq(movies_path)
-    expect(page).not_to have_text(movie.title)
+    expect(page).not_to have_text(movie.description)
   end
 end
